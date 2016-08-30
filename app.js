0bf22a3ae7c6c4586107d7400a6cc7d21acafd1c
Joe van der Walt <joe@kohde.io>
2016-08-30 06:38:22 +0200

    add app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+const webdriverio = require('webdriverio')
+var argv = require('yargs').argv;
+
+var options
+if (argv.chrome) {
+    options = {
+        desiredCapabilities: {
+            browserName: 'chrome', chromeOptions: {
+                prefs: {
+                    'profile.default_content_setting_values.notifications': 2
+                }
+            }
+        },
+        port: argv.port,
+        logOutput: '~/log/fk/iqtags.crawler/'
+    }
+}
+else {
+    options = {
+        desiredCapabilities: {
+            browserName: 'firefox'
+        },
+        port: port,
+        logOutput: '/var/log/fk/iqtags.crawler/'
+    }
+}
+const browser = webdriverio.remote(options)
